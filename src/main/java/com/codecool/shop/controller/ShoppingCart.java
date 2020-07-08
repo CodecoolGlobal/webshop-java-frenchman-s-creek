33fe366835f5e5a584ebf0b53c0380d0460b0c0d
TudorDan <tudor.ist@gmail.com>
2020-07-08 21:52:36 +0300

    Add Task 6.3 (Implement doGet method for displaying cart items)

--- src/main/java/com/codecool/shop/controller/ShoppingCart.java
+++ src/main/java/com/codecool/shop/controller/ShoppingCart.java
@@ -1,10 +1,13 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.dao.OrderDao;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.OrderDaoMem;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.model.Order;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -22,19 +25,16 @@ public class ShoppingCart extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ProductDao productDataStore = ProductDaoMem.getInstance();
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        //ProductDao productDataStore = ProductDaoMem.getInstance();
+        //ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        OrderDao orderDataStore = OrderDaoMem.getInstance();
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-        context.setVariable("category", productCategoryDataStore.find(1));
-        context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
-        // // Alternative setting of the template context
-        // Map<String, Object> params = new HashMap<>();
-        // params.put("category", productCategoryDataStore.find(1));
-        // params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
-        // context.setVariables(params);
+
+        //context.setVariable("category", productCategoryDataStore.find(1));
+        //context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
+        context.setVariable("order", orderDataStore.find(1));
         engine.process("product/shopping-cart.html", context, resp.getWriter());
     }
-
 }
