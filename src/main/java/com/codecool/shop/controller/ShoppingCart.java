9cd57c8c56cff207edc30b12be99f1e158709446
TudorDan <tudor.ist@gmail.com>
2020-07-09 13:43:03 +0300

    Add products quantity change from cart

--- src/main/java/com/codecool/shop/controller/ShoppingCart.java
+++ src/main/java/com/codecool/shop/controller/ShoppingCart.java
@@ -8,6 +8,7 @@ import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.model.Order;
+import com.codecool.shop.model.Product;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -35,6 +36,36 @@ public class ShoppingCart extends HttpServlet {
         //context.setVariable("category", productCategoryDataStore.find(1));
         //context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
         context.setVariable("order", orderDataStore.find(1));
+        engine.process("product/shopping-cart.html", context, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        OrderDao orderDataStore = OrderDaoMem.getInstance();
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        //context.setVariable("category", productCategoryDataStore.find(1));
+        context.setVariable("products", productDataStore.getAll());
+
+
+        //get first order
+        Order order = orderDataStore.find(1);
+
+        String prodIdString = req.getParameter("prodId");
+        Product product = productDataStore.find(Integer.parseInt(prodIdString));
+
+        String prodQuantityString = req.getParameter("prodQuantity");
+        int quantity = Integer.parseInt(prodQuantityString);
+        order.remove(product);
+        for (int i = 1; i <= quantity; i++) {
+            order.add(product);
+        }
+
+        context.setVariable("order", order);
         engine.process("product/shopping-cart.html", context, resp.getWriter());
     }
 }
